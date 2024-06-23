3bb1d8313ef939a58f6146885ddfb8c25be057c6
Flavio Carvalho <124106382+Flavsc@users.noreply.github.com>
2024-06-22 21:17:11 -0300

    testing husky

--- App.tsx
+++ App.tsx
@@ -73,7 +73,7 @@ function App(): React.JSX.Element {
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
-          <Section title="See Your Changes">
+          <Section title='See Your Changes'>
             <ReloadInstructions />
           </Section>
           <Section title="Debug">
